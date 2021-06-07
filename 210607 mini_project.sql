9d95cc62065677814c5fa40b7b8229980e97fc31
Ikk_git <83528634+Siho02@users.noreply.github.com>
2021-06-07 14:48:04 +0900

    Add files via upload

--- 210607 mini_project.sql
+++ 210607 mini_project.sql
@@ -21,7 +21,7 @@ SET ECHO OFF
 rem CONGDON    Invoked in RDBMS at build time.	 29-DEC-1988
 rem OATES:     Created: 16-Feb-83
 
-rem 팀원 : 조태익, 이진의, 장동기, 류선영
+rem 팀원 : 손정현, 이진의, 이진영, 정주영B, 조태익
 
 GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
 ALTER USER SCOTT DEFAULT TABLESPACE USERS;
@@ -29,91 +29,18 @@ ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
 CONNECT SCOTT/TIGER
 
 
-DROP TABLE BROADCAST;
-CREATE TABLE BROADCAST
-       (BNAME VARCHAR2(50),
-	BKIND VARCHAR2(50),
-	BPERSON1 VARCHAR2(10),
-	BPERSON2 VARCHAR2(10),
-	BPERSON3 VARCHAR2(10),
-	CONSTRAINT PK_BROADCAST_BNAME PRIMARY KEY (BNAME));
-	
-INSERT INTO BROADCAST VALUES 
-	('무한도전', '예능', '유재석', '김연아', '서장훈');
-INSERT INTO BROADCAST VALUES 
-	('해피투게더', '예능', '신동엽', '유재석', '이효리');
-INSERT INTO BROADCAST VALUES 
-	('패밀리가 떴다', '예능', '유재석', '이효리', '윤종신');
-INSERT INTO BROADCAST VALUES 
-	('런닝맨', '예능', '유재석', '지석진', '김종국');
-INSERT INTO BROADCAST VALUES 
-	('슈가맨', '예능', '유재석', '산다라박', '유희열');
-Insert into BROADCAST VALUES 
-	('범인은 바로 너!', '예능', '유재석', '안재욱', '김종민');
-Insert into BROADCAST VALUES 
-	('유퀴즈온더블럭', '예능', '유재석', '조세호', NULL);
-Insert into BROADCAST VALUES 
-	('놀면뭐하니?', '예능', '유재석', '지석진', '조세호');
-Insert into BROADCAST VALUES 
-	('미운우리새끼', '예능', '신동엽', '김태우', '김신영');
-Insert into BROADCAST VALUES 
-	('불후의 명곡', '예능', '신동엽', '서장훈', '김건모');
-INSERT INTO BROADCAST VALUES
-	('나는 대한민국', '예능', '신동엽', '김연아', NULL);
-INSERT INTO BROADCAST VALUES
-	('아는 형님', '예능', '강호동', '서장훈', '김희철');
-INSERT INTO BROADCAST VALUES 
-	('골목식당', '예능', '백종원', '김성주', '금새록');
-INSERT INTO BROADCAST VALUES 
-	('손세이셔널', '다큐', '손흥민', NULL, NULL);
-INSERT INTO BROADCAST VALUES 
-	('스트리트푸드파이터', '다큐', '백종원', NULL, NULL);
-
-
-DROP TABLE P_JOB;
-CREATE TABLE P_JOB
-	(
-	JCODE NUMBER(3)	,
-	JNAME VARCHAR2(20)  ,
-	CONSTRAINT PK_P_JOB_JCODE PRIMARY KEY (JCODE)
+DROP TABLE PERSON_INFO;
+CREATE TABLE PERSON_INFO(
+	NO NUMBER(3),
+	NAME VARCHAR2(30),
+	AGE INTEGER(3),
+	HOBBY VARCHAR2(30),
+	INTEREST VARCHAR2(30)
 	);
 	
-INSERT INTO P_JOB VALUES (01, '방송인');
-INSERT INTO P_JOB VALUES (02, '운동선수');
-INSERT INTO P_JOB VALUES (03, '요리연구가');
+INSERT INTO BROADCAST VALUES 
+	(1, '조태익', 29, 'cook', 'back end');
 
-
-DROP TABLE PERSONAL_INFO;
-CREATE TABLE PERSONAL_INFO 
-       (NAMECODE NUMBER(3),
-	NAME VARCHAR2(10) ,
-	BRITHYEAR NUMBER(4),
-	GENDER CHAR(1) CONSTRAINT CH_PERSONAL_INFO_GENDER CHECK (GENDER IN ('F', 'M')),
-	JCODE NUMBER(2),
-	MBTI VARCHAR2(4),
-	WORK1 VARCHAR2(50),
-	WORK2 VARCHAR2(50),
-	WORK3 VARCHAR2(50),
-	WIN VARCHAR2(50),
-	CONSTRAINT PK_PERSONAL_INFO_NAMECODE PRIMARY KEY (NAMECODE),
-	CONSTRAINT FK_PERSONAL_INFO_WORK1 FOREIGN KEY (WORK1) REFERENCES Broadcast(BNAME),
-	CONSTRAINT FK_PERSONAL_INFO_WORK2 FOREIGN KEY (WORK2) REFERENCES Broadcast(BNAME),
-	CONSTRAINT FK_PERSONAL_INFO_WORK3 FOREIGN KEY (WORK3) REFERENCES Broadcast(BNAME),
-	CONSTRAINT FK_PERSONAL_INFO_JOB FOREIGN KEY (JCODE) REFERENCES P_Job(JCODE)
-	);
-	
-INSERT INTO PERSONAL_INFO VALUES 
-	(001, '유재석', 1972, 'M', 01, 'ISFP', '무한도전', '해피투게더', '유퀴즈온더블럭', '백상예술대상 TV부문 대상');
-INSERT INTO PERSONAL_INFO VALUES 
-	(002, '김연아', 1990, 'F', 02, 'ISTP', '무한도전', '런닝맨', '나는 대한민국', '밴쿠버 동계올림픽 싱글 금메달');
-INSERT INTO PERSONAL_INFO VALUES
-	(003, '신동엽', 1971, 'M', 01, 'INTJ', '해피투게더', '미운우리새끼','나는 대한민국', NULL);
-INSERT INTO PERSONAL_INFO VALUES 
-	(004, '서장훈', 1974, 'M', 01, 'INTJ', '아는 형님', '미운우리새끼', '무한도전', '백상예술대상 남자예능상');
-INSERT INTO PERSONAL_INFO VALUES 
-	(005, '손흥민', 1992, 'M', 02, 'ESTP', '아는 형님', '손세이셔널', NULL, '국제축구연맹 푸스카스상');
-INSERT INTO PERSONAL_INFO VALUES
-	(006, '백종원', 1966, 'M', 03, NULL, '골목식당', '스트리트푸드파이터', NULL, 'MBC방송연예대상 PD상');
 
 COMMIT;
 
